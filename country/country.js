3faeeffeda504ecf53b2f5d1bb599813c52594b9
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-24 11:04:52 +0600

    Update country.js

--- country/country.js
+++ country/country.js
@@ -1,1 +1,16 @@
-console.log('first')
+const countryList = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res=>res.json())
+        .then(data=>displayCountry(data))
+}
+
+const displayCountry = country =>{
+    //console.log(country)
+
+    // forEach for load All data's from country Apis
+    country.forEach(countries => {
+        console.log(countries)
+    });
+}
+
+countryList();
