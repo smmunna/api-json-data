030ced83a5387f7d674a1fe60ca62ef36f1d4a15
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-24 15:10:26 +0600

    update

--- meals/meal.js
+++ meals/meal.js
@@ -5,13 +5,28 @@ const getMeal = () =>{
 }
 // Display Meals
 const displayMeals = (meals) =>{
-    console.log(meals)
+  
+    const mealList = meals.meals;
     
     // Main DIV;
     const colListDiv = document.getElementById('col-list');
 
-    // Making the DIV;
-    
+    mealList.forEach(meals=>{
+        console.log(meals)
+        // Making the DIV;
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+        <div class="card h-100">
+            <img src="${meals.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+        </div>
+    `
+    colListDiv.appendChild(div);
+    })
 }
 // First Function Call;
 getMeal();
